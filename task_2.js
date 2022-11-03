d44abafd34ee020feac6ac82cd73af29ee0ebe21
Vladislav122-kor <85311952+Vladislav122-kor@users.noreply.github.com>
2022-11-03 13:33:49 +0300

    feat: make changes in task_2

--- task_2.js
+++ task_2.js
@@ -1,35 +1,71 @@
-function checkValue(value_1, value_2) {
+function checkValue(value, type) {
 
-  if (value_1 === '' || value_2 === '' || value_1 === null || value_2 === null || value_2.includes(' ')) {
-    console.log('Incorrect input!');
-    return;
+  if (value === null) {
+    return 'cancel';
   }
 
-  if (value_2.length > 1) {
-    if (value_2.startsWith('0')) {
+  if (type === 'number') {
+    if (value === '' || value.includes(' ')) {
       console.log('Incorrect input!');
-      return;
+      return false;
+    }
+
+    if (value.length > 1) {
+      if (value.startsWith('0')) {
+        console.log('Incorrect input!');
+        return false;
+      }
+    }
+
+    // converting variable to type number and make additional validations
+    value = Number(value);
+
+    if (Number.isNaN(value) || value < 0 || value > 10) {
+      console.log('Incorrect input!');
+      return false;
+    }
+  } else if (type === 'string') {
+    if (value.length > 3) {
+      console.log('Incorrect input!');
+      return false;
     }
   }
 
-  // converting the second argument to type number and call the function to check the values for compliance with the requirements
-  makeValidation(value_1, Number(value_2));
+  return value;
+} 
+
+
+
+function getValues() {
+
+  let string = checkValue(prompt('Enter up to three any characters (a space counts as a character)'), 'string');
+
+  if (string === 'cancel' || !string) {
+    return;
+  }
+
+  let number = checkValue(prompt('Enter any number from 0 to 10 inclusive'), 'number');
+
+  if (number === 'cancel' || number !== 0 && !number) {
+    return;
+  }
+
+  consoleResult(string, number);
 }
 
-function makeValidation(string, number) {
 
-  if (string.length > 3 || Number.isNaN(number) || number < 0 || number > 10) {
-    console.log('Incorrect input!');
-  } else {
-    const MATRIX = [];
+
+function consoleResult(string, number) {
+  
+  const MATRIX = [];
 
     while (MATRIX.length < number) {
         MATRIX.push(Array(number).fill(string));
     }
 
     console.log(MATRIX);
-
-  }
 }
+  
 
-checkValue(prompt('Enter up to three any characters (a space counts as a character)'), prompt('Enter any number from 0 to 10 inclusive'));
+
+getValues();
