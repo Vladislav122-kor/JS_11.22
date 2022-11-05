7cadd6186e69332fe8577594f93a45ae80c36535
Vladislav Korolyov <vladislav.korolyov1996@gmail.com>
2022-11-05 04:26:20 +0300

    feat: finish task_2

---
+++ task_2.js
@@ -0,0 +1,14 @@
+function selectFromInterval(arr, val_1, val_2) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+
+  arr.forEach((item) => {
+    if (typeof item !== 'number' || !Number.isFinite(item)) {
+      throw new Error();
+    }
+  })
+
+  return val_1 <= val_2 ? arr.filter((item) => item >= val_1 && item <= val_2) : arr.filter((item) => item <= val_1 && item >= val_2);
+}
