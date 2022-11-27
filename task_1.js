d8a3f59500f8ae4d9f35ad3b80a50d43b092ce48
Vladislav122-kor <85311952+Vladislav122-kor@users.noreply.github.com>
2022-11-27 17:02:36 +0300

    Update task_1.js

--- task_1.js
+++ task_1.js
@@ -5,13 +5,9 @@ Array.prototype.customFilter = function(callback, obj = null) {
   if (obj && typeof obj !== 'object' || Array.isArray(obj)) {
     throw new Error('The second argument is not an object or is array');
   }
-  if (obj) {
-    callback.call(obj);
-  }
-
   const RESULT = [];
   for (let i = 0; i < this.length; i += 1) {
-    if (callback(this[i], i, this)) {
+    if (callback.call(obj, this[i], i, this)) {
       RESULT.push(this[i]);
     }
   }
